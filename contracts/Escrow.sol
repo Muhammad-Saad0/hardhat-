0621e409424b3e7004deb020d1dc98a14c17a469
Muhammad-Saad0 <119536957+Muhammad-Saad0@users.noreply.github.com>
2023-07-09 05:23:29 +0500

    NFT transfer added

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -1,6 +1,29 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-contract Escrow{
-    
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+contract Escrow {
+    uint256 nftID;
+    address nftAddress;
+    address seller;
+    address buyer;
+
+    constructor(
+        address _nftAddress,
+        uint256 _nftID,
+        address _seller,
+        address _buyer
+    ) {
+        nftID = _nftID;
+        nftAddress = _nftAddress;
+        seller = _seller;
+        buyer = _buyer;
+    }
+
+    function transferNFT() external {
+        IERC721 nft = IERC721(nftAddress);
+        require(nft.ownerOf(nftID) == seller, "Not the owner of the NFT");
+        nft.transferFrom(seller, buyer, nftID);
+    }
 }
--- test/RealEstate.js
+++ test/RealEstate.js
@@ -1,44 +1,53 @@
-// const { expect } = require("chai");
-
-// describe("Real Estate", () => {
-//   let RealEstate, Escrow;
-
-//   beforeEach(async () => {
-//     const realEstate =
-//       await ethers.getContractFactory(
-//         "RealEstate"
-//       );
-//     const escrow =
-//       await ethers.getContractFactory("Escrow");
-//     RealEstate = await realEstate.deploy();
-//     Escrow = await escrow.deploy();
-//   });
-
-//   describe("Deployment", async () => {});
-// });
-
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("RealEstate", function () {
-  let realEstate;
+  let realEstate, escrow, deployer;
+  let nftID = 1;
 
-  beforeEach(async function () {
+  beforeEach(async () => {
+    //GETTING EVERYONE WHO SIGNS THE CONTRACT
+    const accounts = await ethers.getSigners();
+    //FIRST SIGNER IS THE DEPLOYER
+    deployer = accounts[0];
+    seller = deployer;
+    // HARDHAT SIMULATES THE REAL BLOCKCHAIN AND GIVES US
+    // MULTIPLES SIGNERS FOR TESTING
+    buyer = accounts[1];
     const RealEstate =
       await ethers.getContractFactory(
         "RealEstate"
       );
+    const Escrow =
+      await ethers.getContractFactory("Escrow");
     realEstate = await RealEstate.deploy();
-    await realEstate.deployed();
+    escrow = await Escrow.deploy(
+      realEstate.address,
+      nftID,
+      seller.address,
+      buyer.address
+    );
+    const transaction = await realEstate
+      .connect(seller)
+      .approve(escrow.address, nftID);
+    await transaction.wait();
   });
 
   it("should have minted an NFT", async function () {
-    // Check the owner of the token
+    // OWNER OF THE NFT WITH ID 1 (THAT WILL BE OUT DEPLOYER)
     const owner = await realEstate.ownerOf(1);
     expect(owner).to.equal(
-      await (
-        await ethers.getSigner()
-      ).getAddress()
+      await deployer.getAddress()
     );
+  });
+
+  describe("Transfer", () => {
+    it("Transfer an NFT", async () => {
+      await escrow.transferNFT();
+      const owner = await realEstate.ownerOf(1);
+      expect(owner).to.equal(
+        await buyer.getAddress()
+      );
+    });
   });
 });
