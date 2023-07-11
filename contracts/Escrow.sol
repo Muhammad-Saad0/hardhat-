d667c4e9ffa5d0a29eb1f95ad368f00b7fa82b55
Muhammad-Saad0 <119536957+Muhammad-Saad0@users.noreply.github.com>
2023-07-11 06:54:58 +0500

    added escrow payment functionality and wrote test for it

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -1,29 +1,41 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 contract Escrow {
-    uint256 nftID;
-    address nftAddress;
-    address seller;
-    address buyer;
+    uint256 public nftID;
+    address public nftAddress;
+    uint256 public purchasePrice;
+    uint256 public escrowPrice;
+    address payable public seller;
+    address payable public buyer;
+    address public inspector;
 
     constructor(
         address _nftAddress,
         uint256 _nftID,
-        address _seller,
-        address _buyer
+        address payable _seller,
+        address payable _buyer,
+        uint256 _purchasePrice,
+        uint256 _escrowPrice,
+        address _inspector
     ) {
         nftID = _nftID;
         nftAddress = _nftAddress;
         seller = _seller;
         buyer = _buyer;
+        purchasePrice = _purchasePrice;
+        escrowPrice = _escrowPrice;
+        inspector = _inspector;
     }
 
-    function transferNFT() external {
+    function payEscrowAmount() public payable{
+        require(msg.sender == buyer);
+    }
+
+    function finalizeTransaction() public payable {
         IERC721 nft = IERC721(nftAddress);
-        require(nft.ownerOf(nftID) == seller, "Not the owner of the NFT");
         nft.transferFrom(seller, buyer, nftID);
     }
 }
--- test/RealEstate.js
+++ test/RealEstate.js
@@ -1,52 +1,139 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("RealEstate", function () {
-  let realEstate, escrow, deployer;
-  let nftID = 1;
+// describe("RealEstate", function () {
+//   let realEstate, escrow, deployer;
+//   let nftID = 1;
 
+//   beforeEach(async () => {
+//     //GETTING EVERYONE WHO SIGNS THE CONTRACT
+//     const accounts = await ethers.getSigners();
+//     //FIRST SIGNER IS THE DEPLOYER
+//     deployer = accounts[0];
+//     seller = deployer;
+//     // HARDHAT SIMULATES THE REAL BLOCKCHAIN AND GIVES US
+//     // MULTIPLES SIGNERS FOR TESTING
+//     buyer = accounts[1];
+//     const RealEstate =
+//       await ethers.getContractFactory(
+//         "RealEstate"
+//       );
+//     const Escrow =
+//       await ethers.getContractFactory("Escrow");
+//     realEstate = await RealEstate.deploy();
+//     escrow = await Escrow.deploy(
+//       realEstate.address,
+//       nftID,
+//       seller.address,
+//       buyer.address
+//     );
+//     const transaction = await realEstate
+//       .connect(seller)
+//       .approve(escrow.address, nftID);
+//     await transaction.wait();
+//   });
+
+//   it("should have minted an NFT", async function () {
+//     // OWNER OF THE NFT WITH ID 1 (THAT WILL BE OUT DEPLOYER)
+//     const owner = await realEstate.ownerOf(1);
+//     expect(owner).to.equal(
+//       await deployer.getAddress()
+//     );
+//   });
+
+//   describe("Transfer", () => {
+//     it("Transfer an NFT", async () => {
+//       await escrow.connect(buyer)
+//       const owner = await realEstate.ownerOf(1);
+//       expect(owner).to.equal(
+//         await buyer.getAddress()
+//       );
+//     });
+//   });
+// });
+
+const priceInEther = (purchasePriceEther) => {
+  const purchasePriceWei =
+    ethers.utils.parseEther(
+      purchasePriceEther.toString()
+    );
+  return purchasePriceWei;
+};
+
+describe("Real Estate", () => {
+  let realEstate,
+    escrow,
+    accounts,
+    deployer,
+    seller,
+    buyer,
+    inspector;
+  //THE ID OF FIRST NFT WILL BE 1
+  let nftID = 1;
   beforeEach(async () => {
-    //GETTING EVERYONE WHO SIGNS THE CONTRACT
-    const accounts = await ethers.getSigners();
-    //FIRST SIGNER IS THE DEPLOYER
+    accounts = await ethers.getSigners();
     deployer = accounts[0];
     seller = deployer;
-    // HARDHAT SIMULATES THE REAL BLOCKCHAIN AND GIVES US
-    // MULTIPLES SIGNERS FOR TESTING
     buyer = accounts[1];
+    inspector = accounts[2];
     const RealEstate =
-      await ethers.getContractFactory(
-        "RealEstate"
-      );
+      ethers.getContractFactory("RealEstate");
+    realEstate = await (
+      await RealEstate
+    ).deploy();
     const Escrow =
       await ethers.getContractFactory("Escrow");
-    realEstate = await RealEstate.deploy();
     escrow = await Escrow.deploy(
       realEstate.address,
       nftID,
       seller.address,
-      buyer.address
+      buyer.address,
+      priceInEther(100),
+      priceInEther(20),
+      inspector.address
     );
-    const transaction = await realEstate
+    //WE ARE CONNECTING THE SELLER WHO OWNS THE NFT IN
+    //REALESTATE CONTRACT AND APPROVES THE ESCROW CONTRACT
+    //TO TRANSFER THAT NFT
+    await realEstate
       .connect(seller)
       .approve(escrow.address, nftID);
-    await transaction.wait();
   });
 
-  it("should have minted an NFT", async function () {
-    // OWNER OF THE NFT WITH ID 1 (THAT WILL BE OUT DEPLOYER)
-    const owner = await realEstate.ownerOf(1);
-    expect(owner).to.equal(
-      await deployer.getAddress()
-    );
+  describe("Deployment", () => {
+    it("mints an nft successfully", async () => {
+      const owner = await realEstate.ownerOf(
+        nftID
+      );
+      expect(owner).to.equal(deployer.address);
+    });
   });
 
   describe("Transfer", () => {
-    it("Transfer an NFT", async () => {
-      await escrow.transferNFT();
-      const owner = await realEstate.ownerOf(1);
-      expect(owner).to.equal(
-        await buyer.getAddress()
+    it("Transfers an NFT", async () => {
+      await escrow
+        .connect(buyer)
+        .finalizeTransaction();
+      const owner = await realEstate.ownerOf(
+        nftID
+      );
+      expect(owner).to.equal(buyer.address);
+    });
+
+    it("Sending the down payment", async () => {
+      await escrow
+        .connect(buyer)
+        .payEscrowAmount({
+          value: priceInEther(20),
+        });
+      const balance =
+        await ethers.provider.getBalance(
+          escrow.address
+        );
+      const balanceInEther =
+        ethers.utils.formatEther(balance);
+      expect(parseInt(balanceInEther)).to.equal(
+        20
       );
     });
   });
